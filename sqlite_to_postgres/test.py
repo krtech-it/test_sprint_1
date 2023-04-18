824b2671c6a3106005a2f1c0ead1e79293bee02b
Кирилл Петухов <test@mail.ru>
2023-04-18 22:29:55 +0300

    проверка результата

--- sqlite_to_postgres/test.py
+++ sqlite_to_postgres/test.py
@@ -12,7 +12,9 @@ db_path = 'db.sqlite'
 
 with conn_context(db_path) as conn:
     cur = conn.cursor()
-    cur.execute('select * from film_work where id = ""')
+    cur.execute('select * from film_work where id = "ffc3df9f-a17e-4bae-b0b6-c9c4da290fdd"')
+    movie = cur.fetchone()
+    print(movie)
     # curs = conn.cursor()
     # curs.execute("select * from film_work order by id limit 5;")
     # data = curs.fetchall()
