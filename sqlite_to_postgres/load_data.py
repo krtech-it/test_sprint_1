36e218f58919393c2ec327ff3819b16d68045e9a
Kirill Petyhov <kirill@mail.ru>
2023-04-18 16:18:12 +0300

    довали абстракции

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -12,8 +12,8 @@ from dataclasses import dataclass, field
 TABLE_NAME_METHODS = {
     'genre': 'genre_insert',
     'genre_film_work': 'genre_film_work_insert',
-    'person_film_work': 'person_film_work_select',
-    'person': 'person_select',
+    'person_film_work': 'person_film_work_insert',
+    'person': 'person_insert',
     'film_work': 'film_work_insert',
 }
 
@@ -28,6 +28,7 @@ class FilmWork:
     created_at: datetime.datetime = field(default_factory=datetime.datetime.now)
     modified_at: datetime.datetime = field(default_factory=datetime.datetime.now)
 
+
 @dataclass()
 class Table:
     name: str
@@ -40,15 +41,9 @@ class Table:
                 on conflict (id) do nothing;
                 """
 
-    def genre_insert(self, cursor, pg_conn):
-        dict_fields = {
-            'id': 'id',
-            'name': 'name',
-            'description': 'description',
-            'created_at': 'created',
-            'updated_at': 'modified'
-        }
-        ignore_fields = []
+    def __transfer_data_to_psql(self, cursor, pg_connect, dict_fields, ignore_fields=None):
+        if ignore_fields is None:
+            ignore_fields = []
         cursor.execute(self.SQL_SELECT_HEADER.format(self.name))
         headers = [i[0] for i in cursor.description if i[0] not in ignore_fields]
         cursor.execute(self.SQL_SELECT.format(', '.join(headers), self.name))
@@ -60,16 +55,32 @@ class Table:
             sql_text = self.SQL_INSERT.format(table_name=self.name, headers=headers, args=args)
             pg_cursor.execute(sql_text)
 
-    def genre_film_work_insert(self, cursor, pg_conn):
+    def genre_insert(self, cursor, pg_connect):
+        dict_fields = {
+            'id': 'id',
+            'name': 'name',
+            'description': 'description',
+            'created_at': 'created',
+            'updated_at': 'modified'
+        }
+        self.__transfer_data_to_psql(cursor, pg_conn, dict_fields)
+
+    def genre_film_work_insert(self, cursor, pg_connect):
         pass
 
-    def person_film_work_insert(self, cursor, pg_conn):
-        print('person_film_work_insert')
+    def person_film_work_insert(self, cursor, pg_connect):
+        pass
 
-    def person_insert(self, cursor, pg_conn):
-        print('person_insert')
+    def person_insert(self, cursor, pg_connect):
+        dict_fields = {
+            'id': 'id',
+            'full_name': 'full_name',
+            'created_at': 'created',
+            'updated_at': 'modified'
+        }
+        self.__transfer_data_to_psql(cursor, pg_connect, dict_fields)
 
-    def film_work_insert(self, cursor, pg_conn):
+    def film_work_insert(self, cursor, pg_connect):
         dict_fields = {
             'id': 'id',
             'title': 'title',
@@ -81,16 +92,8 @@ class Table:
             'updated_at': 'modified'
         }
         ignore_fields = ['file_path']
-        cursor.execute(self.SQL_SELECT_HEADER.format(self.name))
-        headers = [i[0] for i in cursor.description if i[0] not in ignore_fields]
-        cursor.execute(self.SQL_SELECT.format(', '.join(headers), self.name))
-        data = cursor.fetchall()
-        values_s = '(' + ', '.join(['%s'] * len(headers)) + ')'
-        headers = '(' + ', '.join([dict_fields[i] for i in headers]) + ')'
-        with pg_conn.cursor() as pg_cursor:
-            args = ','.join(pg_cursor.mogrify(values_s, item).decode() for item in data)
-            sql_text = self.SQL_INSERT.format(table_name=self.name, headers=headers, args=args)
-            pg_cursor.execute(sql_text)
+        self.__transfer_data_to_psql(cursor, pg_connect, dict_fields, ignore_fields)
+
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
